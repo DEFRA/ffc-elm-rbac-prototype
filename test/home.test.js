a7d1634e5308a974f76cd38a350cd56d4dd46214
markysoft <markharrop@yahoo.com>
2019-03-29 11:37:31 +0000

    add code from api boilerplate add scripts for start, start-watch and unit-test-watch update README.md with project info

---
+++ test/home.test.js
@@ -0,0 +1,37 @@
+const Lab = require('lab')
+const Code = require('code')
+const lab = exports.lab = Lab.script()
+const createServer = require('../server')
+
+lab.experiment('home test', () => {
+  let server
+
+  // Create server before each test
+  lab.before(async () => {
+    server = await createServer()
+  })
+
+  lab.test('GET / route works', async () => {
+    const options = {
+      method: 'GET',
+      url: '/'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(200)
+    Code.expect(response.result).to.equal({ hello: 'world' })
+  })
+
+  lab.test('GET unknown route returns 404 error with statusCode, error, and message', async () => {
+    const options = {
+      method: 'GET',
+      url: '/noSuchRoute'
+    }
+
+    const response = await server.inject(options)
+    Code.expect(response.statusCode).to.equal(404)
+    Code.expect(response.result.statusCode).to.equal(404)
+    Code.expect(response.result.error).to.equal('Not Found')
+    Code.expect(response.result.message).to.equal('Not Found')
+  })
+})
