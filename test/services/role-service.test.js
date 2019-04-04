68287039acf3c82a9d9ee949b58f923a47ede8e6
Matthew Collins <matthew.collins@defra.gov.uk>
2019-04-04 09:46:21 +0100

    Some changes to the data to move roles into plan specific roles. Added a public user.

--- test/services/role-service.test.js
+++ test/services/role-service.test.js
@@ -12,7 +12,7 @@ lab.experiment('role service', () => {
   })
 
   lab.test('get elm user credentials for no plans gives plan.create permission', async () => {
-    const credentials = service.getCredentials('user04')
+    const credentials = service.getCredentials('user01')
     Code.expect(credentials).to.exist()
     Code.expect(credentials.roles).to.exist()
     Code.expect(credentials.roles.length).to.equal(1)
@@ -41,7 +41,7 @@ lab.experiment('role service', () => {
   })
 
   lab.test('get user credentials for unknown plan ID returns only create', async () => {
-    const credentials = service.getCredentials('user04', 'nosuchplan')
+    const credentials = service.getCredentials('user01', 'nosuchplan')
     Code.expect(credentials).to.exist()
     Code.expect(credentials.roles).to.exist()
     Code.expect(credentials.roles.length).to.equal(1)
@@ -61,6 +61,28 @@ lab.experiment('role service', () => {
     Code.expect(credentials.permissions[0].name).to.equal('plan.create')
     Code.expect(credentials.permissions[1].name).to.equal('plan.approve')
     Code.expect(credentials.permissions[2].name).to.equal('plan.review')
+    console.log(JSON.stringify(credentials, undefined, 2))
+  })
+
+  lab.test('get credentials for public user with no plan', async () => {
+    const credentials = service.getCredentials('user04')
+    Code.expect(credentials).to.exist()
+    Code.expect(credentials.roles).to.exist()
+    Code.expect(credentials.roles.length).to.equal(1)
+    Code.expect(credentials.roles[0].name).to.equal('PublicUser')
+    Code.expect(credentials.permissions.length).to.equal(1)
+    Code.expect(credentials.permissions[0].name).to.equal('plan.public')
+    console.log(JSON.stringify(credentials, undefined, 2))
+  })
+
+  lab.test('get credentials for public user with plan', async () => {
+    const credentials = service.getCredentials('user04', 'plan01')
+    Code.expect(credentials).to.exist()
+    Code.expect(credentials.roles).to.exist()
+    Code.expect(credentials.roles.length).to.equal(1)
+    Code.expect(credentials.roles[0].name).to.equal('PublicUser')
+    Code.expect(credentials.permissions.length).to.equal(1)
+    Code.expect(credentials.permissions[0].name).to.equal('plan.public')
     console.log(JSON.stringify(credentials, undefined, 2))
   })
 })
